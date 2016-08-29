1a93d51044a2bb23d1089d3fc26dafba6613d731
TsukasaDesu <Tsuka0627t@yahoo.co.jp>
2016-08-29 17:07:35 +0900

    見下ろし方カメラかんせい
    GitHubなかなかわからん

--- Player/PlayerBehaviour.cs
+++ Player/PlayerBehaviour.cs
@@ -17,6 +17,10 @@ public class PlayerBehaviour : MonoBehaviour {
     public Text money_text;
     public Slider hp_gage;
     public Text result_time_txt;
+    public float height_subcam = 100;
+
+    GameObject tpscamera;
+    GameObject subcamera;
 
     Rigidbody rigid;
 
@@ -28,7 +32,10 @@ public class PlayerBehaviour : MonoBehaviour {
         power = 30;
         rigid = GetComponent<Rigidbody>();
         result_time = 0;
-	}
+        tpscamera = GameObject.Find("TPSCamera");
+        subcamera = GameObject.Find("SubCamera");
+        Camera.main.depth = 1;
+    }
 
     // Update is called once per frame
     void Update() {
@@ -36,25 +43,61 @@ public class PlayerBehaviour : MonoBehaviour {
         result_time_txt.text = string.Format("{0:00}", Mathf.Floor(result_time / 60)) + ":" + string.Format("{0:00}", Mathf.Floor(result_time) % 60) + ":" + string.Format("{0:00}",(result_time - Mathf.Floor(result_time))*100);
         money_text.text = "所持金:" + money + "円";
         hp_gage.value = hp;
-        rigid.velocity = new Vector3(Camera.main.transform.forward.x,0,Camera.main.transform.forward.z) * Input.GetAxis("Vertical")*10+ Camera.main.transform.right * Input.GetAxis("Horizontal") * 10;
-
+        rigid.velocity = ((tpscamera.tag == "MainCamera") ? new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z):new Vector3(Camera.main.transform.up.x,0,Camera.main.transform.up.z)) * Input.GetAxis("Vertical") * 10 + Camera.main.transform.right * Input.GetAxis("Horizontal") * 10;
+        subcamera.transform.position = transform.position + new Vector3(0, height_subcam, 0);
         if (Input.GetMouseButtonDown(0))
         {
             if (!GameMasterBehaviour.NextFlg) return;
-            GameObject bul = Instantiate(bullet, transform.position + Camera.main.transform.forward, Quaternion.Euler(transform.forward)) as GameObject;
-            bul.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * 50;
-            Destroy(bul, 1);
+
+            GameObject bul;
+            if(tpscamera.tag == "MainCamera")
+            {
+                bul = Instantiate(bullet, transform.position + Camera.main.transform.forward, Quaternion.Euler(transform.forward)) as GameObject;
+                bul.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * 50;
+                Destroy(bul, 1);
+            }
+            else
+            {
+                Vector3 mousepos = Input.mousePosition;
+                mousepos.z = Camera.main.transform.position.z;
+                Vector3 mousepos_world = Camera.main.ScreenToWorldPoint(mousepos);
+                Debug.Log((mousepos_world - transform.position).normalized);
+                bul = Instantiate(bullet, transform.position + new Vector3(transform.position.x-mousepos_world.x,0,transform.position.z-mousepos_world.z).normalized*2, Quaternion.Euler(transform.forward)) as GameObject;
+                bul.GetComponent<Rigidbody>().velocity = new Vector3(transform.position.x - mousepos_world.x, 0, transform.position.z - mousepos_world.z).normalized * 50;
+                Destroy(bul, 1);
+
+            }
+
+
         }
 
-        if(hp <= 0)
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            if(tpscamera.tag == "MainCamera")
+            {
+                Camera.main.depth = 0;
+                subcamera.tag = "MainCamera";
+                tpscamera.tag = "Untagged";
+                Camera.main.depth = 1;
+            }
+            else
+            {
+                Camera.main.depth = 0;
+                subcamera.tag = "Untagged";
+                tpscamera.tag = "MainCamera";
+                Camera.main.depth = 1;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            height_subcam += Input.GetAxis("Mouse ScrollWheel") * 100;
+            height_subcam = Mathf.Clamp(height_subcam, 10.0f, 200.0f);
+        }
+
+         if (hp <= 0)
         {
             SceneManager.LoadScene(1);
         }
-
-        //ExecuteEvents.Execute<IRecieveMessage>(
-        //                    target: hit.collider.gameObject,
-        //                    eventData: null,
-        //                    functor: (recievetarget, y) => recievetarget.OnRecieve(gun[no].power)
-        //                    );
     }
 }
